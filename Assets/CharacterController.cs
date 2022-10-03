de09656ec010901ffaa9bfe03e954c1119bbf473
Pewners <jfadikpe2021@elam.co.uk>
2022-10-03 09:19:51 +0100

    trying to fix sliding issue

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Controls : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     public float maxSpeed;
     public float normalSpeed = 10.0f;
     //public float sprintSpeed = 40.0f;
     //public float maxSprint = 5.0f;
-    //float sprintTimer;
+    //public float sprintTimer;
 
     float rotation = 0.0f;
     float camRotation = 0.0f;
@@ -17,7 +18,7 @@ public class Controls : MonoBehaviour
     GameObject cam;
     Rigidbody myRigidBody;
 
-    //bool isOnGround;
+    bool isOnGround;
     public GameObject groundChecker;
     public LayerMask groundLayer;
     //public float jumpForce = 500.0f;
@@ -39,32 +40,31 @@ public class Controls : MonoBehaviour
 
     void Update()
     {
-        //isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
         //myAnim.SetBool("isOnGround", isOnGround);
 
         //if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
         //{
-        //    myAnim.SetTrigger("jumped");
-        //    myRigidBody.AddForce(transform.up * jumpForce);
+        //myAnim.SetTrigger("jumped");
+        //myRigidBody.AddForce(transform.up * jumpForce);
         //}
 
         //if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
         //{
-        //    maxSpeed = sprintSpeed;
-        //    sprintTimer = sprintTimer - Time.deltaTime;
-        //}
-        //else
+        //maxSpeed = sprintSpeed;
+        //sprintTimer = sprintTimer - Time.deltaTime;
+        //} else
         //{
-        //    maxSpeed = normalSpeed;
-        //    if (Input.GetKey(KeyCode.LeftShift) == false)
-        //    {
-        //        sprintTimer = sprintTimer + Time.deltaTime;
-        //    }
+        //maxSpeed = normalSpeed;
+        //if (Input.GetKey(KeyCode.LeftShift) == false)
+        //{
+        //sprintTimer = sprintTimer + Time.deltaTime;
+        //}
         //}
 
         //sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
 
-        Vector3 newVelocity = ((transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed));
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed));
         myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
 
         //myAnim.SetFloat("speed", newVelocity.magnitude);
@@ -74,5 +74,20 @@ public class Controls : MonoBehaviour
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
+
+        //if (Input.GetKey(KeyCode.Escape))
+        //{
+        //    SceneManager.LoadScene(5);
+        //}
+    }
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.CompareTag("Drug"))
+        {
+            normalSpeed = normalSpeed + 7f;
+            maxSpeed = maxSpeed + 7f;
+            Debug.Log("Speed");
+        }
     }
 }
