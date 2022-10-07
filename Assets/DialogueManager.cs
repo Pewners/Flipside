58e5e44f29cc290a5e4561bc5a6692304d975050
Pewners <jfadikpe2021@elam.co.uk>
2022-10-07 15:13:46 +0100

    started on dialogue

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -8,16 +9,42 @@ public class DialogueManager : MonoBehaviour
     public GameObject text;
     bool inTrigger = false;
 
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int index;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        textComponent.text = string.Empty;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("e pressed");
+            text.SetActive(false);
+            StartDialogue();
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (textComponent.text == lines[index])
+                {
+                    NextLine();
+                }
+
+                else
+                {
+                    StopAllCoroutines();
+                    textComponent.text = lines[index];
+
+                }
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -25,6 +52,35 @@ public class DialogueManager : MonoBehaviour
         {
             text.SetActive(true);
             inTrigger = true;
+        }
+    }
+
+    void StartDialogue()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
         }
     }
 
