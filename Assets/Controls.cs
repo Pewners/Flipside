d51bba2066856cef82f55b9499c4983354283eb2
Pewners <jfadikpe2021@elam.co.uk>
2022-10-13 23:17:25 +0100

    trying out dialogue tutorial

--- Assets/Controls.cs
+++ Assets/Controls.cs
@@ -1,78 +1,65 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
 
-public class Controls : MonoBehaviour
+public class @Controls : IInputActionCollection, IDisposable
 {
-    public float maxSpeed;
-    public float normalSpeed = 10.0f;
-    //public float sprintSpeed = 40.0f;
-    //public float maxSprint = 5.0f;
-    //float sprintTimer;
-
-    float rotation = 0.0f;
-    float camRotation = 0.0f;
-    float rotationSpeed = 2.0f;
-    float camRotationSpeed = 1.5f;
-    GameObject cam;
-    Rigidbody myRigidBody;
-
-    //bool isOnGround;
-    public GameObject groundChecker;
-    public LayerMask groundLayer;
-    //public float jumpForce = 500.0f;
-
-    //Animator myAnim;
-
-    void Start()
+    public InputActionAsset asset { get; }
+    public @Controls()
     {
-        //myAnim = GetComponentInChildren<Animator>();
-
-        //Cursor.lockState = CursorLockMode.Locked;
-
-        //sprintTimer = maxSprint;
-
-        cam = GameObject.Find("Main Camera");
-        myRigidBody = GetComponent<Rigidbody>();
+        asset = InputActionAsset.FromJson(@"
+{
+    ""name"": ""Controls"",
+    ""maps"": [],
+    ""controlSchemes"": []
+}");
     }
 
-
-    void Update()
+    public void Dispose()
     {
-        //isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
-        //myAnim.SetBool("isOnGround", isOnGround);
+        UnityEngine.Object.Destroy(asset);
+    }
 
-        //if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    myAnim.SetTrigger("jumped");
-        //    myRigidBody.AddForce(transform.up * jumpForce);
-        //}
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
 
-        //if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
-        //{
-        //    maxSpeed = sprintSpeed;
-        //    sprintTimer = sprintTimer - Time.deltaTime;
-        //}
-        //else
-        //{
-        //    maxSpeed = normalSpeed;
-        //    if (Input.GetKey(KeyCode.LeftShift) == false)
-        //    {
-        //        sprintTimer = sprintTimer + Time.deltaTime;
-        //    }
-        //}
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
 
-        //sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
 
-        Vector3 newVelocity = ((transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed));
-        myRigidBody.velocity = new Vector3(newVelocity.x, myRigidBody.velocity.y, newVelocity.z);
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
 
-        //myAnim.SetFloat("speed", newVelocity.magnitude);
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
 
-        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
 
-        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
-        cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
     }
 }
